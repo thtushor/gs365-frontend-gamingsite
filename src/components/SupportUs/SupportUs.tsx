0eecfb32152d3ed43ea2de99bac00630ecd975da
Sohidul-Islam <sishufol.sim@gmail.com>
2025-06-01 12:47:51 +0600

    fest

--- src/components/SupportUs/SupportUs.tsx
+++ src/components/SupportUs/SupportUs.tsx
@@ -2,27 +2,11 @@ import React, { useState } from "react";
 import "./SupportUs.scss";
 import { Logo } from "../Logo/Logo";
 import SupportPanel from "./SupportPanel";
-import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 
 const BUTTON_SIZE = 64;
 
 const SupportUs: React.FC = () => {
-  const [position, setPosition] = useState({
-    x: window.innerWidth - BUTTON_SIZE - 30,
-    y: window.innerHeight - BUTTON_SIZE - 100,
-  });
-  const [isDragging, setIsDragging] = useState(false);
   const [open, setOpen] = useState(false);
-
-  const handleDragStart = () => {
-    setIsDragging(true);
-  };
-
-  const handleDragStop = (e: DraggableEvent, data: DraggableData) => {
-    console.log({ e });
-    setIsDragging(false);
-    setPosition({ x: data.x, y: data.y });
-  };
 
   const handleClick = () => {
     setOpen(true);
@@ -31,7 +15,6 @@ const SupportUs: React.FC = () => {
   const handleClose = () => {
     setOpen(false);
   };
-  // co;
 
   return (
     <>
@@ -39,39 +22,26 @@ const SupportUs: React.FC = () => {
       <div
         style={{
           position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          pointerEvents: "none",
+          bottom: "100px",
+          right: "30px",
+          zIndex: 1000,
         }}
       >
-        <Draggable
-          handle=".support-us-floating-button"
-          bounds="parent"
-          position={position}
-          onStart={handleDragStart}
-          onStop={handleDragStop}
-          defaultPosition={{ x: position.x, y: position.y }}
+        <button
+          className="support-us-floating-button"
+          style={{
+            width: BUTTON_SIZE,
+            height: BUTTON_SIZE,
+            cursor: "pointer",
+            userSelect: "none",
+          }}
+          onClick={handleClick}
+          aria-label="Support Us"
         >
-          <div style={{ position: "absolute", pointerEvents: "auto" }}>
-            <button
-              className="support-us-floating-button"
-              style={{
-                width: BUTTON_SIZE,
-                height: BUTTON_SIZE,
-                cursor: isDragging ? "grabbing" : "grab",
-                userSelect: "none",
-              }}
-              onClick={handleClick}
-              aria-label="Support Us"
-            >
-              <div className="icon-container">
-                <Logo />
-              </div>
-            </button>
+          <div className="icon-container">
+            <Logo />
           </div>
-        </Draggable>
+        </button>
       </div>
     </>
   );
