262c0a5059ea8f1b33dbf4612fdfced4e7ad74f8
mdmahfuzrp <mdmahfuzrp@gmail.com>
2025-07-07 11:36:34 +0600

    fix

--- src/pages/SingleDeposit.jsx
+++ src/pages/SingleDeposit.jsx
@@ -5,6 +5,7 @@ import BaseModal from "../components/Promotion/BaseModal";
 import { LuGift } from "react-icons/lu";
 import DepositPromotionSelect from "../components/InnerComponent/DepositPromotionSelect";
 import bankCard from "../assets/bank-card.png";
+import { MdOutlineRadioButtonChecked } from "react-icons/md";
 
 const fakePromotions = {
   valid: [
@@ -149,13 +150,43 @@ const SingleDeposit = () => {
           <p htmlFor="" className="text-base text-left mb-2 mt-4">
             Select Payment
           </p>
-          <div className="border border-yellow-400 overflow-hidden flex items-center justify-center flex-col rounded-md max-w-[200px] p-3 pt-4 relative">
+          <div className="border group border-yellow-400 cursor-pointer overflow-hidden flex items-center justify-center flex-col rounded-md max-w-[200px] p-3 pt-4 relative">
             <span className="absolute top-0 right-0 bg-yellow-400 rounded-bl-md px-2 text-black text-[14px] font-semibold">
               100%
             </span>
             <img className="w-[30px]" src={bankCard} />
-            <p>Local Bank</p>
+            <p className="group-hover:text-yellow-400 font-medium">
+              Local Bank
+            </p>
           </div>
+        </div>
+        <div>
+          <p htmlFor="" className="text-base text-left mb-2 mt-4">
+            Select Type
+          </p>
+          <div className="second-bg border border-yellow-400 hover:text-yellow-400 cursor-pointer transition px-5 py-4 pr-3 rounded-md text-[18px] font-semibold flex items-center w-full gap-2 justify-between">
+            <span className="flex items-center gap-2">Bank Transfer</span>
+            <span className="flex items-center gap-3 text-[24px] text-yellow-400">
+              <MdOutlineRadioButtonChecked />
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <p htmlFor="" className="text-base text-left mb-2 mt-4">
+            Deposit Channel
+          </p>
+          <select
+            className="border border-yellow-400 rounded px-3 py-2 second-bg font-medium outline-none"
+            name="status"
+            required
+          >
+            <option value="City Bank">City Bank</option>
+            <option value="Islami Bank">Islami Bank</option>
+          </select>
+        </div>
+
+        <div className="header-auth mt-5">
+          <button className="signup-btn w-full h-[45px]">Continue</button>
         </div>
       </div>
 
