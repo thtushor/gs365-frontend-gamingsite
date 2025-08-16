9d2eb6e7c1449b9a924d90dfbec8dc06611de1af
thtushor <hasankhanltd@gmail.com>
2025-08-16 15:00:31 +0600

    Update API base URL for production and revert local URL to commented state

--- src/lib/api/apiClient.js
+++ src/lib/api/apiClient.js
@@ -5,9 +5,8 @@ import axios from "axios";
 import { useAuth } from "../../contexts/auth-context";
 
 // Base URL for API requests
-// export const BASE_URL = "https://gs-server-svmt.onrender.com";
-export const BASE_URL = "https://glorypos.com/gs-server";
-// export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "https://glorypos.com/gs-server";
+export const BASE_URL = "http://localhost:3000";
 export const API_LIST = {
   GET_HERO_BANNERS: "/api/public/banners-list",
   GET_ANNOUNCEMENTS: "/api/public/announcement",
